2f54aafc90691613111ca6c22d76a4a6cbde8451
priyanakhre <priyanakhre@gmail.com>
2017-02-11 23:26:39 -0500

    start and end button

---
+++ app/src/main/java/com/example/hulagirl/fitspiration/EndButton.java
@@ -0,0 +1,86 @@
+package com.example.hulagirl.fitspiration;
+
+import android.annotation.SuppressLint;
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Date;
+
+public class EndButton extends AppCompatActivity {
+
+    private TextView tvDay, tvHour, tvMinute, tvSecond, tvEvent;
+    private LinearLayout linearLayout1, linearLayout2;
+    private Handler handler;
+    private Runnable runnable;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_button);
+
+
+        initUI();
+        countDownStart();
+    }
+
+
+    @SuppressLint("SimpleDateFormat")
+    private void initUI() {
+        linearLayout1 = (LinearLayout) findViewById(R.id.ll1);
+        linearLayout2 = (LinearLayout) findViewById(R.id.ll2);
+
+        tvHour = (TextView) findViewById(R.id.txtTimerHour);
+        tvMinute = (TextView) findViewById(R.id.txtTimerMinute);
+        tvSecond = (TextView) findViewById(R.id.txtTimerSecond);
+        tvEvent = (TextView) findViewById(R.id.tvevent);
+    }
+
+    // //////////////COUNT DOWN START/////////////////////////
+    public void countDownStart() {
+        handler = new Handler();
+        runnable = new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void run() {
+                handler.postDelayed(this, 1000);
+                try {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat(
+                            "yyyy-MM-dd");
+                    // Here Set your Event Date
+                    Date eventDate = dateFormat.parse("2017-02-12");
+                    Date currentDate = new Date();
+                    if (!currentDate.after(eventDate)) {
+                        long diff = eventDate.getTime()
+                                - currentDate.getTime();
+                        long days = diff / (24 * 60 * 60 * 1000);
+                        diff -= days * (24 * 60 * 60 * 1000);
+                        long hours = diff / (60 * 60 * 1000);
+                        diff -= hours * (60 * 60 * 1000);
+                        long minutes = diff / (60 * 1000);
+                        diff -= minutes * (60 * 1000);
+                        long seconds = diff / 1000;
+                        tvDay.setText("" + String.format("%02d", days));
+                        tvHour.setText("" + String.format("%02d", hours));
+                        tvMinute.setText("" + String.format("%02d", minutes));
+                        tvSecond.setText("" + String.format("%02d", seconds));
+                    } else {
+                        linearLayout1.setVisibility(View.VISIBLE);
+                        linearLayout2.setVisibility(View.GONE);
+                        tvEvent.setText("Android Event Start");
+                        handler.removeCallbacks(runnable);
+                        // handler.removeMessages(0);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        handler.postDelayed(runnable, 0);
+    }
+}
--- app/src/main/java/com/example/hulagirl/fitspiration/signInButtons.java
+++ app/src/main/java/com/example/hulagirl/fitspiration/signInButtons.java
@@ -1,13 +1,33 @@
 package com.example.hulagirl.fitspiration;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class signInButtons extends AppCompatActivity {
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_buttons);
+
+        login = (Button) findViewById(R.id.button3);
+
+        // Capture button clicks
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(signInButtons.this,
+                        HomeScreen.class);
+                startActivity(myIntent);
+            }
+
+        });
+
+
     }
 }
