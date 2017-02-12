902e40dd6de47dd39c30dd350cc16be9c5634e0c
Deanna <Deanna>
2017-02-11 19:05:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/hulagirl/fitspiration/signInButtons.java
@@ -0,0 +1,13 @@
+package com.example.hulagirl.fitspiration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signInButtons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_buttons);
+    }
+}
